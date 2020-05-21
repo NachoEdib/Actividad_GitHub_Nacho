68bc3e129e7c2f3875fe0935444435a6119116d7
Usuario <Usuario@DESKTOP-DEP70FL>
2020-05-21 14:50:20 +0200

    Commit prueba

--- src/main/java/com/mycompany/actividad_github_nacho/Principal.java
+++ src/main/java/com/mycompany/actividad_github_nacho/Principal.java
@@ -17,6 +17,11 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola");
         System.out.println("Hola2");
+        System.out.println("Hola2");
+        System.out.println("Hola2");
+        System.out.println("Hola2");
+        System.out.println("Hola2");
+        
     }
     
 }
